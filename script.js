6146485860dd6e29c73095e6198142d04d4e7fc3
Among2108 <oassalam2108@gmail.com>
2026-02-14 05:52:53 +0700

    10%

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const items = document.querySelectorAll("#navMenu .nav-item");
+
+items.forEach((item, index) => {
+
+  item.addEventListener("mouseenter", () => {
+
+    items.forEach((el, i) => {
+
+      el.classList.remove(
+        "scale-150",
+        "-translate-x-8",
+        "translate-x-8",
+        "font-bold"
+      );
+
+      if (i === index) {
+        el.classList.add("scale-150", "font-bold"); // ขยายใหญ่
+      } 
+      else if (i < index) {
+        el.classList.add("-translate-x-8"); // ซ้ายถอยซ้าย
+      } 
+      else {
+        el.classList.add("translate-x-8"); // ขวาถอยขวา
+      }
+
+    });
+
+  });
+
+  item.addEventListener("mouseleave", () => {
+    items.forEach((el) => {
+      el.classList.remove(
+        "scale-150",
+        "-translate-x-8",
+        "translate-x-8",
+           "font-bold"
+      );
+    });
+  });
+
+});
 
 
 
